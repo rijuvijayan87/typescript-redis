a844e08319641d728160792c915709c83ee05237
Riju Vijayan <rvijayan@zscaler.com>
2025-02-15 23:55:12 +0530

    chore: zod and middleware

--- routes/restaurants.ts
+++ routes/restaurants.ts
@@ -1,6 +1,9 @@
 import express from "express";
+import { validate } from "../middlewares/validate.js";
+import { RestaurantSchema, type Restaurant } from "../schemas/restaurants.js";
 export const router = express.Router();
 
-router.get("/", async (req, res) => {
+router.post("/", validate(RestaurantSchema), async (req, res, next) => {
+  const data = req.body as Restaurant;
   res.send("hey from restaurants");
 });
